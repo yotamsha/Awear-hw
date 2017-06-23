55fe94931b5a26fb1bb9080ae36d88a8715748bf
yotam shalev <yotamsha@gmail.com>
2017-06-23 16:24:20 +0300

    changed server port

--- server/server.js
+++ server/server.js
@@ -9,6 +9,6 @@ const app = express();
 app.use(express.static(path.join(__dirname, './../client')));
 routes.init(app);
 // start the server
-app.listen(8000, () => {
-    console.log('Server is running on http://localhost:8000 or http://127.0.0.1:8000');
+app.listen(80, () => {
+    console.log('Server is running on http://localhost:80 or http://127.0.0.1:80');
 });
